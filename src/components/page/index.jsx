af397dbe3fe792c3a08f85150012687f63f5b8ad
CezarNogueira <cezartv1221@gmail.com>
2024-03-22 21:21:05 -0300

    Adição final de 22/03

--- src/components/page/index.jsx
+++ src/components/page/index.jsx
@@ -1,13 +1,41 @@
 import React from "react";
+import { useState } from "react";
 import './css/Listas_module.css';
 
 function Listas () {
+    const [text, setText] = useState('');
+    const [task, setTask] = useState([]);
+    const clearTask = useState();
+
+    const createTask = () => {
+        if(text !== '') {
+            setTask ([...task, { text: text}]);
+            setText ('');
+        };
+    };
+
     return (
-            <div className="grid_wrapper_listas">
+            <div className="flex_wrapper_listas">
 
-                <div className="box_atividade">
-                </div>
+                <ol>
+                    <div className="flex_wrapper_task">
 
+                        <div className="box_task">
+                            {task.map((task, index) => (
+                            <li key={index}>{task.text}</li>
+                            ))};
+                        </div>
+                        
+                    </div>
+                </ol>
+                
+                <div className="input_wrapper">
+                    <input type="text" value={text} onChange={(e) => setText(e.target.value)}/>
+
+                    <button onClick={createTask}>ADD TASK</button>
+                    <button onClick={clearTask}>CLEAR</button>
+                </div> 
+                      
             </div>
     );
 }
